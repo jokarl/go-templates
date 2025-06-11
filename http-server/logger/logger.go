b2cba1656854fdea950eb2dd29d50e5e81e45fd1
Johan Karlsson <johan.a.karlsson_c@sandvik.com>
2025-06-11 16:32:43 +0200

    Allow context logging in logger package

--- http-server/logger/logger.go
+++ http-server/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"os"
 )
@@ -8,9 +9,13 @@ import (
 // Logger defines the interface for logging operations.
 type Logger interface {
 	Debug(msg string, args ...any)
+	DebugContext(ctx context.Context, msg string, args ...any)
 	Info(msg string, args ...any)
+	InfoContext(ctx context.Context, msg string, args ...any)
 	Warn(msg string, args ...any)
+	WarnContext(ctx context.Context, msg string, args ...any)
 	Error(msg string, args ...any)
+	ErrorContext(ctx context.Context, msg string, args ...any)
 }
 
 // slogger is an implementation of the Logger interface using slog.
@@ -20,22 +25,31 @@ type slogger struct {
 
 // New creates a new Logger instance with a specified minimum log level.
 func New(level slog.Leveler) Logger {
-	logger := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-		Level: level,
-	}))
 	return &slogger{
-		logger: logger,
+		logger: slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+			Level: level,
+		})),
 	}
 }
 
-// Debug logs a message at DebugLevel.
+// Debug logs a message at slog.LevelDebug.
 func (l *slogger) Debug(msg string, args ...any) {
 	l.logger.Debug(msg, args...)
 }
 
-// Info logs a message at InfoLevel.
+// DebugContext logs a message at slog.LevelDebug with a context.
+func (l *slogger) DebugContext(ctx context.Context, msg string, args ...any) {
+	l.logger.DebugContext(ctx, msg, args...)
+}
+
+// Info logs a message at slog.LevelInfo.
 func (l *slogger) Info(msg string, args ...any) {
 	l.logger.Info(msg, args...)
+}
+
+// InfoContext logs a message at slog.LevelInfo with a context.
+func (l *slogger) InfoContext(ctx context.Context, msg string, args ...any) {
+	l.logger.InfoContext(ctx, msg, args...)
 }
 
 // Warn logs a message at WarnLevel.
@@ -43,7 +57,17 @@ func (l *slogger) Warn(msg string, args ...any) {
 	l.logger.Warn(msg, args...)
 }
 
+// WarnContext logs a message at slog.LevelWarn with a context.
+func (l *slogger) WarnContext(ctx context.Context, msg string, args ...any) {
+	l.logger.WarnContext(ctx, msg, args...)
+}
+
 // Error logs a message at ErrorLevel.
 func (l *slogger) Error(msg string, args ...any) {
 	l.logger.Error(msg, args...)
+}
+
+// ErrorContext logs a message at slog.LevelError with a context.
+func (l *slogger) ErrorContext(ctx context.Context, msg string, args ...any) {
+	l.logger.ErrorContext(ctx, msg, args...)
 }
